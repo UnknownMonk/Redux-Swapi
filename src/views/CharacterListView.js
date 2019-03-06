87d7ae9e762b6d7aa10722b5fe4e5a65f464319c
UnknownMonk <smodcartoon@gmail.com>
2019-03-06 11:42:41 -0500

    Added actions and wired up CharacterListView.js to accept props and actions

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,21 +1,20 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { CharacterList } from "../components";
+import { CharacterList } from '../components';
 // import actions
+import { fetchData } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
     // call our action
+    this.props.fetchData();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <h1>loading</h1>;
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +24,16 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    characters: state.charsReducer.characters,
+    fetching: state.charsReducer.fetching
+  };
+};
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { fetchData }
 )(CharacterListView);
